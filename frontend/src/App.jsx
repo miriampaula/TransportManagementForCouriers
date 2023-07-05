211c976200b827b1fdcbed5b76efff337f8c5d35
DenisDragos1 <dragosdenisdanut@yahoo.com>
2023-07-05 21:17:00 +0300

    Inregistrare

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import DosarTransportPage from "./pages/DosarTransportPage";
 import HomePage from "./pages/HomePage";
+import Navbar from "./components/Navbar";
+import LoginPage from "./pages/LoginPage"
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Navbar/>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/dosar-transport" element={<DosarTransportPage />} />
+          <Route path="/login" element={<LoginPage/>}/>
+          <Route path="/register" element={<RegisterPage/>}/>
         </Routes>
       </Router>
     </div>
