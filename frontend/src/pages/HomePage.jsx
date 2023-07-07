0bba85bdae7245bdd5eeebf3a438ca5295d331f5
Andreea <dragosandreea1@gmail.com>
2023-07-07 09:53:12 +0300

    commit

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -4,8 +4,8 @@ import Scanner from '../components/Scanner';
 export default function HomePage() {
   return (
     <div>
-      <Navbar />
+      
       <Scanner />
-    </body>
+    </div>
   );
 };
