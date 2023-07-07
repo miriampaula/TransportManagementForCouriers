03ed615008a8cef4aa6e4dcc168928a2c5e4c729
Cristi Cureu <cristi.cureu@indecosoft.ro>
2023-07-07 09:56:11 +0300

    register

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { useEffect, useState } from "react";
 import Button from "../components/Button";
 
@@ -11,18 +10,6 @@ const RegisterPage = () => {
   });
   const [error, setError] = useState("");
 
-=======
-import { useState } from "react";
-import Button from "../components/Button";
- 
-const RegisterPage = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [passwordRepeat, setPasswordRepeat] = useState("");
-  const [error, setError] = useState("");
- 
->>>>>>> 0e1d6f5ba342fa047ae279f92abb082d3a3c07f8
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
@@ -33,11 +20,7 @@ const RegisterPage = () => {
           headers: {
             "Content-Type": "application/json",
           },
-<<<<<<< HEAD
           body: JSON.stringify(userData),
-=======
-          body: JSON.stringify({ name, email, password, passwordRepeat }),
->>>>>>> 0e1d6f5ba342fa047ae279f92abb082d3a3c07f8
         }
       );
       const resposneText = await response.text();
@@ -51,15 +34,11 @@ const RegisterPage = () => {
       console.log("RegisterPage::handleSubmit::", error.message);
     }
   };
-<<<<<<< HEAD
 
   useEffect(() => {
     console.log(userData);
   }, [userData]);
 
-=======
- 
->>>>>>> 0e1d6f5ba342fa047ae279f92abb082d3a3c07f8
   return (
     <div className="flex w-full  justify-center p-4">
       <form className="flex flex-col items-center justify-around border-gray-200 border-2 rounded-md w-3/4 lg:w-2/4 h-96">
@@ -69,11 +48,7 @@ const RegisterPage = () => {
           placeholder="Name"
           required
           autoComplete="off"
-<<<<<<< HEAD
           onChange={(e) => setUserData({ ...userData, name: e.target.value })}
-=======
-          onChange={(e) => setName(e.target.value)}
->>>>>>> 0e1d6f5ba342fa047ae279f92abb082d3a3c07f8
         />
         <input
           className=" lg:w-3/4  border-gray-400 border-2 text-gray-900 outline-none text-sm rounded-md p-3 focus:border-sky-500"
@@ -81,11 +56,7 @@ const RegisterPage = () => {
           placeholder="Email"
           required
           autoComplete="off"
-<<<<<<< HEAD
           onChange={(e) => setUserData({ ...userData, email: e.target.value })}
-=======
-          onChange={(e) => setEmail(e.target.value)}
->>>>>>> 0e1d6f5ba342fa047ae279f92abb082d3a3c07f8
         />
         <input
           className=" lg:w-3/4  border-gray-400 border-2 text-gray-900 outline-none text-sm rounded-md p-3 focus:border-sky-500"
@@ -93,13 +64,9 @@ const RegisterPage = () => {
           placeholder="Password"
           required
           autoComplete="off"
-<<<<<<< HEAD
           onChange={(e) =>
             setUserData({ ...userData, password: e.target.value })
           }
-=======
-          onChange={(e) => setPassword(e.target.value)}
->>>>>>> 0e1d6f5ba342fa047ae279f92abb082d3a3c07f8
         />
         <input
           className=" lg:w-3/4  border-gray-400 border-2 text-gray-900 outline-none text-sm rounded-md p-3 focus:border-sky-500"
@@ -107,13 +74,9 @@ const RegisterPage = () => {
           placeholder="Confirm password"
           required
           autoComplete="off"
-<<<<<<< HEAD
           onChange={(e) =>
             setUserData({ ...userData, passwordRepeat: e.target.value })
           }
-=======
-          onChange={(e) => setPasswordRepeat(e.target.value)}
->>>>>>> 0e1d6f5ba342fa047ae279f92abb082d3a3c07f8
         />
         <Button text="register" onClick={handleSubmit} />
         {error ? <div className="text-red-600">{error}</div> : null}
@@ -121,10 +84,5 @@ const RegisterPage = () => {
     </div>
   );
 };
-<<<<<<< HEAD
 
 export default RegisterPage;
-=======
- 
-export default RegisterPage;
->>>>>>> 0e1d6f5ba342fa047ae279f92abb082d3a3c07f8
