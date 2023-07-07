d85c9e7398cc2081133b870c8ea7f388ab091680
Tintas Gheorghe <gheorghetintas.14@gmail.com>
2023-07-07 09:23:23 +0300

    modificari login

---
+++ frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,72 @@
+import { Link } from "react-router-dom";
+import Button from "../components/Button";
+import { useEffect, useState } from "react";
+ 
+const BASE_URL = "http://localhost:80/api";
+ 
+const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+ 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(`${BASE_URL}/loginUser`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
+      console.log(response);
+      const responseText = await response.text();
+      console.log(responseText);
+      if (response.status === 200) {
+        console.log("success");
+      } else {
+        setError(responseText);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+ 
+  useEffect(() => {
+    console.log("email: ", email);
+    console.log("password: ", password);
+  }, [email, password]);
+ 
+  return (
+    <div className="flex w-full  justify-center p-4">
+      <form className="flex flex-col items-center justify-around border-gray-200 border-2 rounded-md w-3/4 lg:w-2/4 h-96">
+        <input
+          className=" lg:w-3/4  border-gray-400 border-2 text-gray-900 outline-none text-sm rounded-md p-3 focus:border-sky-500"
+          type="text"
+          placeholder="email"
+          required
+          autoComplete="off"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          className="lg:w-3/4 border-gray-400 border-2 text-gray-900 outline-none text-sm rounded-md p-3 focus:border-sky-500"
+          type="password"
+          placeholder="password"
+          required
+          autoComplete="off"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Button text="Login" onClick={handleSubmit} />
+        <div className="flex text-lg">
+          <p>Nu ai cont?</p>
+          <Link className="ml-2 underline text-blue-500" to="/register">
+            Înregistrează-te
+          </Link>
+        </div>
+        {error && <div className="text-red-600">{error}</div>}
+      </form>
+    </div>
+  );
+};
+ 
+export default LoginPage;
