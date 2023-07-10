859785ce60c91ef49413aac826f07f1e9418a625
Andreea <dragosandreea1@gmail.com>
2023-07-10 14:59:37 +0300

    commit

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,11 +1,9 @@
 import { useEffect } from "react";
 import Scanner from "../components/Scanner";
-import sql from "../services/SqlService";
+
 
 export default function HomePage() {
-  useEffect(() => {
-    sql.query('test');
-  }, []);
+
   return (
     <div>
       
