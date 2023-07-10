c19a4bd26e82a8b0c4520bf93c3e06b4ca33e2ec
Antonel Gabriel Crișan <antonel19.gabriel@gmail.com>
2023-07-10 12:28:58 +0300

    update

--- backend/src/router.ts
+++ backend/src/router.ts
@@ -13,7 +13,7 @@ import {
   logoutUser,
   validateToken,
 } from "./user";
-
+import { getStatus, putStatus, deleteStatus  } from "../model/status";
 const bodyParser = koaBody({
   jsonLimit: 50 * 1024 * 1025,
   formLimit: 50 * 1024 * 1025,
@@ -37,6 +37,9 @@ const multipartBody = koaBody({
 const router = new Router({ prefix: "/api" });
 
 router
+  .delete("/data/status", deleteStatus)
+  .get("/data/status", getStatus)
+  .put("/data/status",bodyParser, putStatus)
   .post("/loginUser", bodyParser, loginUser)
   .get("/logoutUser", logoutUser)
   .post("/registerUser", bodyParser, registerUser)
--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Button from "../components/Button";
-
+import Input from "../components/Input";
 const RegisterPage = () => {
   const [userData, setUserData] = useState({
     name: "",
@@ -43,7 +43,7 @@ const RegisterPage = () => {
     <div className="flex w-full  justify-center p-20">
       <form className="flex flex-col items-center justify-around border-gray-200 border-2 rounded-md w-3/4 lg:w-2/4 h-96">
         <h1 className="text-2xl font-bold">Register</h1>
-        <input
+        <Input
           className=" lg:w-2/4  border-gray-400 border-2 text-gray-900 outline-none text-sm rounded-md p-3 focus:border-sky-500"
           type="text"
           placeholder="Name"
@@ -51,36 +51,33 @@ const RegisterPage = () => {
           autoComplete="off"
           onChange={(e) => setUserData({ ...userData, name: e.target.value })}
         />
-        <input
+        <Input
           className=" lg:w-2/4  border-gray-400 border-2 text-gray-900 outline-none text-sm rounded-md p-3 focus:border-sky-500"
           type="text"
           placeholder="Email"
           required
           autoComplete="off"
-          onChange={(e) => setUserData({ ...userData, email: e.target.value })}
+          pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2, 4}$"
+          onChange={(e) => setUserData({ ...userData, name: e.target.value })}
         />
-        <input
+        <Input
           className=" lg:w-2/4  border-gray-400 border-2 text-gray-900 outline-none text-sm rounded-md p-3 focus:border-sky-500"
-          type="password"
+          type="text"
           placeholder="Password"
           required
           autoComplete="off"
-          onChange={(e) =>
-            setUserData({ ...userData, password: e.target.value })
-          }
+          onChange={(e) => setUserData({ ...userData, name: e.target.value })}
         />
-        <input
+        <Input
           className=" lg:w-2/4  border-gray-400 border-2 text-gray-900 outline-none text-sm rounded-md p-3 focus:border-sky-500"
-          type="password"
-          placeholder="Confirm password"
+          type="text"
+          placeholder="Confrim password"
           required
           autoComplete="off"
-          onChange={(e) =>
-            setUserData({ ...userData, passwordRepeat: e.target.value })
-          }
+          onChange={(e) => setUserData({ ...userData, name: e.target.value })}
         />
         <Button text="register" onClick={handleSubmit} />
-        {error ? <div className="text-red-600">{error}</div> : null}
+        {error ? <div className="text-red-600">Please complete all field!</div> : null}
       </form>
     </div>
   );
