d582ad96a2329cbe21fd9497c90438f533aec490
Andreea <dragosandreea1@gmail.com>
2023-07-10 09:04:36 +0300

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import DosarTransportPage from "./pages/DosarTransportPage";
 import HomePage from "./pages/HomePage";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
