e27c593d6c6753fd502dfaf7c5344ad1ca1a7b53
miriampaula <miriamromaniuc@gmail.com>
2023-07-10 12:20:30 +0300

    loginfarasqlservices

--- backend/src/model/status.ts
+++ backend/src/model/status.ts
@@ -9,12 +9,55 @@ export async function putStatus(ctx) {
     const { nume, tipStatus, statusDesign } = ctx.request.body;
     if (!nume) {
         return ctx.throw(400, "Coloana <nume> este obligatorie pentru tabela <status>!");
+    } else if (typeof nume !== "string") {
+        return ctx.throw(400, "Coloana <nume> pentru tabela <status> trebuie sa contina type string!");
     }
     if (!tipStatus) {
         return ctx.throw(400, "Coloana <tipStatus> este obligatorie pentru tabela <status>!");
+    } else if (typeof tipStatus !== "string") {
+        return ctx.throw(400, "Coloana <tipStatus> pentru tabela <status> trebuie sa contina type string!");
     }
+
     if (!statusDesign) {
         return ctx.throw(400, "Coloana <statusDesign> este obligatorie pentru tabela <status>!");
+    } else if (typeof statusDesign !== "string") {
+        return ctx.throw(400, "Coloana <statusDesign> pentru tabela <status> trebuie sa contina type string!");
     }
     ctx.body = await sql("insert into dbo.Status(nume, TipStatus, StatusDesign) values(@nume, @tipStatus, @statusDesign)", ctx.request.body);
+}
+
+
+export async function postStatus(ctx) {
+    const { nume, tipStatus, statusDesign } = ctx.request.body;
+
+    if (nume) {
+        if (typeof nume !== "string") {
+            return ctx.throw(400, "Coloana <nume> pentru tabela <status> trebuie sa contina type string!");
+        } else {
+            ctx.body = await sql("update dbo.Status set nume = @nume,", ctx.request.body);
+        }
+
+    }
+    if (tipStatus) {
+        if (typeof tipStatus !== "string") {
+            return ctx.throw(400, "Coloana <tipStatus> pentru tabela <status> trebuie sa contina type string!");
+        } else {
+            ctx.body = await sql("update dbo.Status set TipStatus = @tipStatus,", ctx.request.body);
+        }
+    }
+    if (statusDesign) {
+        if (typeof statusDesign !== "string") {
+            return ctx.throw(400, "Coloana <statusDesign> pentru tabela <status> trebuie sa contina type string!");
+        } else {
+            ctx.body = await sql("update dbo.Status set StatusDesign = @statusDesign,", ctx.request.body);
+        }
+    }
+}
+
+
+export async function deleteStatus(ctx) {
+    const { nume, tipStatus, statusDesign } = ctx.request.body;
+
+    ctx.body = await sql("delete from dbo.Status where Nume = @nume)", ctx.request.body);
+
 }
--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,17 +1,19 @@
 import { Link, useNavigate } from "react-router-dom";
 import Button from "../components/Button";
 import { useState, useEffect } from "react";
-import sql from "../services/SqlService";
+import Input from "../components/Input";
 
 const BASE_URL = "http://localhost:80/api";
 
 const LoginPage = () => {
   const navigate = useNavigate();
-  const [userData, setUserData] = useState({ email: "", password: "", })
+  const [userData, setUserData] = useState({ email: "", password: "" });
   const [error, setError] = useState("");
-  useEffect(() => {
-    sql.query('test');
-  }, []);
+
+  const onChange = (field, value) => {
+    setUserData({ ...userData, [field]: value });
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
@@ -22,54 +24,49 @@ const LoginPage = () => {
         },
         body: JSON.stringify(userData),
       });
-      console.log(response);
       const responseText = await response.text();
       if (response.status === 200) {
-        console.log("suuces!");
-        alert("Login result.. navigare pe pagina HOME");
+        console.log("success");
+        alert("Login reusit... navigare pe pagina HOME");
         navigate("/");
       } else {
         setError(responseText);
       }
-    } catch (e) {
-      console.log(e);
+    } catch (error) {
+      console.log(error);
     }
-
   };
+
+  useEffect(() => {
+    console.log(userData);
+  }, [userData]);
 
   return (
     <div className="flex w-full justify-center p-4">
-      <form className="flex flex-col items-center justify-around border w-3/4 h-96">
-        <input
-          className="border-gray-400 border-2 text-gray-900 outline-none text-sm rounded-md p-3 focus:border-sky-500"
-          type="text"
+      <form className="flex flex-col items-center justify-around border w-3/4 h-96 px-2">
+        <Input
           placeholder="email"
-          required
-          autoComplete="off"
-          onChange={(e) => {
-            setUserData({ ...userData, email: e.target.value });
-          }}
+          name="email"
+          type="email"
+          onChange={(e) => onChange(e.target.name, e.target.value)}
         />
-        <input
-          className="border-gray-400 border-2 text-gray-900 outline-none text-sm rounded-md p-3 focus:border-sky-500"
-          type="password"
+        <Input
           placeholder="password"
-          required
-          autoComplete="off"
-          onChange={(e) => {
-            setUserData({ ...userData, password: e.target.value });
-          }}
+          name="password"
+          type="password"
+          onChange={(e) => onChange(e.target.name, e.target.value)}
         />
         <Button text="Login" onClick={handleSubmit} />
         <div className="flex text-lg">
           <p>Nu ai cont?</p>
           <Link className="ml-2 underline text-blue-500" to="/register">
-            Register page
+            Înregistrează-te
           </Link>
         </div>
+        {error ? <div className="text-red-600">{error}</div> : null}
       </form>
     </div>
   );
+};
 
-}
 export default LoginPage;
