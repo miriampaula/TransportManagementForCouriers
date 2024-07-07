733bff567782732c5332cb3e28dce072beba3807
miriampaula <miriamromaniuc@gmail.com>
2024-07-07 15:08:56 +0300

    all

--- backend/src/model/colet.ts
+++ backend/src/model/colet.ts
@@ -4,7 +4,7 @@ import { sql } from "./../sql";
 export async function getColet(ctx: Koa.Context) {
   let { idDosar } = ctx.request.query;
   let query = `select c.* from dbo.DosarTransport as d 
-  join dbo.DosarTransport_Facturi as f on d.Id = f.IdDosarTransport and d.Id = @idDosar
+  join  dbo.DosarTransport_Facturi as f on d.Id = f.IdDosarTransport and d.Id = @idDosar
   join dbo.DosarTransport_Colete as c on f.IdFactura = c.IdFactura`;
   let { recordset } = await sql(query, { idDosar } as any);
   ctx.body = recordset;
